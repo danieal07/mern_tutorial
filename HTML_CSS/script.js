5dcafeb94244aa6615b9100067c17419c6cdaeb6
danieal <daniealantony@gmail.com>
2025-02-17 08:08:34 +0530

    day_6_AN

--- HTML_CSS/script.js
+++ HTML_CSS/script.js
@@ -1,10 +1,35 @@
-let a=10;
-a=0.1+10;
+// types of loops
+// 1.for 
+// 2 while
+// 3.do while
+// 4.for in
+// 5 for of
+// 6 for each
+
+
+const arr=[10,20,"hello",true,[40,50,60],{name:"sachin",age:45}];
 object1={
-    name:"John",
-    age:30,
-    
-};
-object2=new Object();
-console.log(object1);
-console.log(object2);
+    name:"sachin",
+    age:45,
+    mark:[10,20,30,40],
+    greet(){
+        console.log("hello");
+    }
+}
+// for
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+// for in (it will return the index)
+// for( i in arr)
+// {
+//     console.log(arr[i]);
+// }
+// for([key,values] of Object.entries(object1))
+// {
+//     console.log(key,values);
+// }
+const a=()=>(console.log("hello"));
+a();
+Object.entries(object1).forEach(([key,value])=>console.log(key,value));
