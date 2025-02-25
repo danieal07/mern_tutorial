57313ac639611667676573d677feadd58d12192b
danieal <daniealantony@gmail.com>
2025-02-25 13:20:27 +0530

    DAY_13_MRNG_BACKENDCONNECT

--- Sjit_Backend/index.js
+++ Sjit_Backend/index.js
@@ -1,12 +1,44 @@
-const express=require("express")
+ const Signup=require('./models/SignupSchema')
+const express=require("express");
+
+const mdb=require('mongoose');
+const dotenv=require('dotenv')
 
 const app=express()
+app.use(express.json())
+const PORT=3001
+dotenv.config()
 
-app.get("/", (req,res)=>{
-    res.send("<h1> Welcome to Backend Server</h1>")
+console.log(process.env.MONGODB_URL);
+mdb.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("Mongo db connection succesfull")
+}).catch((err)=>{
+    console.log("check your connection String",err);
 })
 
-const PORT=3001
+app.get("/static", (req,res)=>{
+    res.sendFile("/Users/danieal/Downloads/sjit2025mern/sjitfrontend/index.html")
+})
+ 
+app.post("/signup",(req,res)=>{
+    try{
+    const{firstName,lastName,email,password,phoneNumber}=req.body
+    const newSignup=new Signup({
+    firstName:"danieal",
+    lastName:"A",
+    email:"daniealantony@gmail.com",
+    password:"1234",
+    phoneNumber:7200062860,
+    })  
+    newSignup.save();
+    console.log("signup succesfully")
+    res.status(201).json({message:"signup succesful",isSignup:true})
+    } 
+   catch(error){
+       console.log(error);
+       res.status(201).json({message:"signup unsuccesful",isSignup:false})
+   }
+});
 
 
 app.listen(PORT,()=>console.log("Server Started Succesfully"));
